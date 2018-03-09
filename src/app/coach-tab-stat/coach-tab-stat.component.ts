a1442c0b8e776145ebc190807b469d0a1ba36332
Roman Martseniuk <roma160498@yandex.ru>
2018-03-09 19:35:06 +0300

    Added no-value validation

--- src/app/coach-tab-stat/coach-tab-stat.component.ts
+++ src/app/coach-tab-stat/coach-tab-stat.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../data.service';
+import { DataService } from '../data.service';
 import { Country } from '../models/countries';
 import { Observable } from 'rxjs/Observable';
 import { League } from '../models/league';
@@ -10,29 +10,29 @@ import { Coach } from '../models/coach';
 @Component({
   selector: 'app-coach-tab-stat',
   templateUrl: './coach-tab-stat.component.html',
-  providers:[DataService],
+  providers: [DataService],
   styleUrls: ['./coach-tab-stat.component.css']
 })
 export class CoachTabStatComponent implements OnInit {
   powers = ['Really Smart', 'Super Flexible',
-  'Super Hot', 'Weather Changer'];
+    'Super Hot', 'Weather Changer'];
   selects = [
     {
-    label:'Country',
-    items:[]
-  },
-  {
-    label:'League',
-    items:[{id:0,name:"APL"},{id:1,name:"AAAA"}]
-  },
-  {
-    label:'Team',
-    items:[{id:0,name:"123qedw"},{id:1,name:"2143534"}]
-  },
-  {
-    label:'Player',
-    items:[{id:0,name:"////////"},{id:1,name:"+++++++++"}]
-  }];
+      label: 'Country',
+      items: []
+    },
+    {
+      label: 'League',
+      items: [{ id: 0, name: "APL" }, { id: 1, name: "AAAA" }]
+    },
+    {
+      label: 'Team',
+      items: [{ id: 0, name: "123qedw" }, { id: 1, name: "2143534" }]
+    },
+    {
+      label: 'Player',
+      items: [{ id: 0, name: "////////" }, { id: 1, name: "+++++++++" }]
+    }];
 
   selectedValues = [];
 
@@ -45,71 +45,70 @@ export class CoachTabStatComponent implements OnInit {
   observableCoaches: Observable<Coach[]>;
   coaches: Coach[] = [];
   private initialObject: Coach = {
-    id:0,
-    name:"Name",
-    surname:"",
-    idStatistic:"",
-    lostMatches:"",
-    winMatches:"",
-    drawMatches:"",
-    titles:"",
-    yearsOld:""
+    id: 0,
+    name: "Name",
+    surname: "",
+    idStatistic: "",
+    lostMatches: "",
+    winMatches: "",
+    drawMatches: "",
+    titles: "",
+    yearsOld: ""
   };
   currentCoach: Coach = this.initialObject;
 
-  constructor(private dataService: DataService){
+  constructor(private dataService: DataService) {
   }
 
-  ngOnInit(){  
+  ngOnInit() {
     this.observableCoutries = this.dataService.getCountryName();
     this.observableCoutries.subscribe(
       (countries) => this.countries = countries
     );
   }
   onCountryChange(event) {
-    this.observableLeagues = this.dataService.getLeagueByCountry(event.target.value);
-    this.observableLeagues.subscribe(
-      (leagues) => this.leagues = leagues
-    );
+    let value = event.target.value;
+    if (value !== '-1') {
+      this.observableLeagues = this.dataService.getLeagueByCountry(value);
+      this.observableLeagues.subscribe(
+        (leagues) => this.leagues = leagues
+      );
+    }
     this.leagues = [];
     this.teams = [];
     this.coaches = [];
     this.currentCoach = this.initialObject;
   }
   onLeagueChange(event) {
-    this.observableTeams = this.dataService.getTeamByLeague(event.target.value);
-    this.observableTeams.subscribe(
-      (teams) => this.teams = teams
-    );
+    let value = event.target.value;
+    if (value !== '-1') {
+      this.observableTeams = this.dataService.getTeamByLeague(value);
+      this.observableTeams.subscribe(
+        (teams) => this.teams = teams
+      );
+    }
     this.teams = [];
     this.coaches = [];
     this.currentCoach = this.initialObject;
   }
   onTeamChange(event) {
-    this.observableCoaches = this.dataService.getCoachByTeam(event.target.value);
-    this.observableCoaches.subscribe(
-      (coaches) => this.coaches = coaches
-    );
+    let value = event.target.value;
+    if (value !== '-1') {
+      this.observableCoaches = this.dataService.getCoachByTeam(value);
+      this.observableCoaches.subscribe(
+        (coaches) => this.coaches = coaches
+      );
+    }
     this.coaches = [];
     this.currentCoach = this.initialObject;
   }
   onCoachChange(event) {
-    this.currentCoach = this.coaches.filter(el => el.id === event.target.value)[0];
-  }
-  onChange(event) {
-    let selectedValue = event.target.value;
-    let selectedType = event.target.getAttribute('id').toLowerCase();
-    let prior;
-    console.log(selectedType);
-    switch (selectedType) {
-      case "country": prior = 0; break;
-      case "league": prior = 1; break;
-      case "team": prior = 2; break;
-      default: prior = 3; break;
+    let value = event.target.value;
+    if (value !== '-1') {
+      this.currentCoach = this.coaches.filter(el => el.id === value)[0];
+    } else {
+      this.currentCoach = this.initialObject;
     }
-    this.selectedValues = this.selectedValues.filter((el)=>el.priority < prior);
-    this.selectedValues.push({type:selectedType,id:selectedValue,priority:prior});
-    console.log(this.selectedValues);
   }
   clearAll() {
     this.leagues = [];
--- src/app/country-select-component-stat/country-select-component-stat.component.ts
+++ src/app/country-select-component-stat/country-select-component-stat.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {DataService} from '../data.service';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 import { Observable } from 'rxjs/Observable';
 import { Country } from '../models/countries';
 import { League } from '../models/league';
@@ -9,10 +9,10 @@ import { Player } from '../models/player';
 @Component({
   selector: 'app-country-select-component-stat',
   templateUrl: './country-select-component-stat.component.html',
-  providers:[DataService],
+  providers: [DataService],
   styleUrls: ['./country-select-component-stat.component.css']
 })
-export class CountrySelectComponentStatComponent  implements OnInit   {
+export class CountrySelectComponentStatComponent implements OnInit {
   observableCoutries: Observable<Country[]>;
   countries: Country[] = [];
   observableLeagues: Observable<League[]>;
@@ -22,59 +22,73 @@ export class CountrySelectComponentStatComponent  implements OnInit   {
   observablePlayers: Observable<Player[]>;
   players: Player[] = [];
   private initialObject: Player = {
-    id:0,
-    name:"Name",
-    surname:"",
-    position:"",
-    idStatistic:"",
-    lostMatches:"",
-    winMatches:"",
-    drawMatches:"",
-    goals:"",
-    keyPasses:"",
-    redCards:"",
-    yellowCards:""
+    id: 0,
+    name: "Name",
+    surname: "",
+    position: "",
+    idStatistic: "",
+    lostMatches: "",
+    winMatches: "",
+    drawMatches: "",
+    goals: "",
+    keyPasses: "",
+    redCards: "",
+    yellowCards: ""
   };
   currentPlayer: Player = this.initialObject;
 
-  constructor(private dataService: DataService){
+  constructor(private dataService: DataService) {
   }
 
-  ngOnInit(){  
+  ngOnInit() {
     this.observableCoutries = this.dataService.getCountryName();
     this.observableCoutries.subscribe(
       (countries) => this.countries = countries
     );
   }
   onCountryChange(event) {
-    this.observableLeagues = this.dataService.getLeagueByCountry(event.target.value);
-    this.observableLeagues.subscribe(
-      (leagues) => this.leagues = leagues
-    );
+    let value = event.target.value;
+    if (value !== '-1') {
+      this.observableLeagues = this.dataService.getLeagueByCountry(value);
+      this.observableLeagues.subscribe(
+        (leagues) => this.leagues = leagues
+      );
+    }
     this.leagues = [];
     this.teams = [];
     this.players = [];
     this.currentPlayer = this.initialObject;
   }
   onLeagueChange(event) {
-    this.observableTeams = this.dataService.getTeamByLeague(event.target.value);
-    this.observableTeams.subscribe(
-      (teams) => this.teams = teams
-    );
+    let value = event.target.value;
+    if (value !== '-1') {
+      this.observableTeams = this.dataService.getTeamByLeague(value);
+      this.observableTeams.subscribe(
+        (teams) => this.teams = teams
+      );
+    }
     this.teams = [];
     this.players = [];
     this.currentPlayer = this.initialObject;
   }
   onTeamChange(event) {
-    this.observablePlayers = this.dataService.getPlayerByTeam(event.target.value);
-    this.observablePlayers.subscribe(
-      (players) => this.players = players
-    );
+    let value = event.target.value;
+    if (value !== '-1') {
+      this.observablePlayers = this.dataService.getPlayerByTeam(value);
+      this.observablePlayers.subscribe(
+        (players) => this.players = players
+      );
+    }
     this.players = [];
     this.currentPlayer = this.initialObject;
   }
   onPlayerChange(event) {
-    this.currentPlayer = this.players.filter(el => el.id === event.target.value)[0];
+    let value = event.target.value;
+    if (value !== '-1') {
+      this.currentPlayer = this.players.filter(el => el.id === value)[0];
+    } else {
+      this.currentPlayer = this.initialObject;
+    }
   }
 
   clearAll() {
